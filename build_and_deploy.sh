a0f896bd2f5155328f6cfed13cc66c2660dfdd4c
Jim Blakley <jimbla9@gmail.com>
2019-03-12 14:29:24 +0000

    Create EKSScannerQS

---
+++ build_and_deploy.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+### 0. Things to do up front (Blakley)
+cd ~/capture
+kubectl apply -f scanner-config.yml # In case of changes -- already done on cluster create
+
+test -z "$AWS_ACCESS_KEY_ID" && \
+	export AWS_ACCESS_KEY_ID=$(grep aws_access_key_id ~/.aws/credentials|cut -f2 -d "=")
+	
+test -z "$AWS_ACCESS_KEY_ID" && \
+	(echo "Could not find AWS_ACCESS_KEY_ID";exit 1)
+
+test -z "$AWS_SECRET_ACCESS_KEY" && \	
+	export AWS_SECRET_ACCESS_KEY=$(grep aws_secret_access_key ~/.aws/credentials|cut -f2 -d "=")
+
+test -z "$AWS_SECRET_ACCESS_KEY" && \
+	(echo "Could not find AWS_SECRET_ACCESS_KEY";exit 1)
+
+
+### 1. Check if container repo exists
+aws ecr describe-repositories --repository-names scanner
+REG_EXISTS=$?
+if [ $REG_EXISTS -ne 0 ]; then
+    # Create container repo
+    aws ecr create-repository --repository-name scanner
+fi
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+# Get container repo URI
+REPO_URI=$(aws ecr describe-repositories --repository-names scanner | jq -r '.repositories[0].repositoryUri')
+echo $REPO_URI
+
+### 2. Build master and worker docker images
+docker pull scannerresearch/scanner:cpu-latest
+
+docker build -t $REPO_URI:scanner-master . \
+       -f Dockerfile.master
+
+docker build -t $REPO_URI:scanner-worker . \
+       -f Dockerfile.worker
+
+aws configure set default.region us-east-1
+
+# Provides an auth token to enable pushing to container repo
+LOGIN_CMD=$(aws ecr get-login --no-include-email)
+eval $LOGIN_CMD
+
+# Push master and worker images
+docker push $REPO_URI:scanner-master
+docker push $REPO_URI:scanner-worker
+
+### 2. Deploy master and worker services
+
+# Create secret for sharing AWS credentials with instances
+kubectl delete secret aws-storage-key 2>/dev/null >/dev/null # raises unnecessary error if key doesn't exist
+kubectl create secret generic aws-storage-key \
+        --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
+# Replace REPO_NAME with the location of the docker image
+sed "s|<REPO_NAME>|$REPO_URI:scanner-master|g" master.yml.template > master.yml
+sed "s|<REPO_NAME>|$REPO_URI:scanner-worker|g" worker.yml.template > worker.yml
+
+# Record existing replicas for worker so we can scale the service after deleting
+REPLICAS=$(kubectl get deployments scanner-worker -o json | jq '.spec.replicas' -r)
+
+# Create the number of replicas == number of k8s nodes minus 1 for the master
+NUMNODES=$(kubectl get nodes -o json| jq -r '.items[].status.addresses[] | select(.type=="InternalIP") | .address'|wc|awk '{print $1}')
+REPLICAS=$(expr $NUMNODES - 1)
+
+# Delete and then redeploy the master and worker services
+kubectl delete deploy --all
+
+# Delete deploy does not get rid of the old load balancer -- they build up over time, so delete it
+LBDNS=$(kubectl get services scanner-master --output json | jq -r '.status.loadBalancer.ingress[0].hostname')
+LBS=$(aws elb describe-load-balancers|jq -r '.LoadBalancerDescriptions[]')
+LBTHIS=$(echo $LBS|jq -r "select(.DNSName==\"$LBDNS\")"|jq -r '.LoadBalancerName')
+test -n "$LBTHIS" && aws elb delete-load-balancer --load-balancer-name $LBTHIS
+
+kubectl delete service scanner-master 2>/dev/null >/dev/null # raises unnecessary error if service doesn't exist
+kubectl create -f master.yml
+kubectl create -f worker.yml
+
+# If there was an existing service, scale the new one back up to the same size
+if [[ "$REPLICAS" ]]; then
+    kubectl scale deployment/scanner-worker --replicas=$REPLICAS
+fi
+
+### 3. Expose the master port for the workers to connect to
+kubectl expose -f master.yml --type=LoadBalancer --target-port=8080 --selector='app=scanner-master'
+
---
+++ create_eks_cluster.sh
@@ -0,0 +1,135 @@
+programname=$0
+
+function usage {
+    echo "usage: $programname name"
+    echo "  name    name to give the cluster"
+    exit 1
+}
+
+if [ $# == 0 ]; then
+    usage
+fi
+
+NAME=$1
+
+VPC_STACK_NAME=eks-vpc
+AWSACCT=601041732504
+
+cd ~/capture
+
+### 1. Create a VPC (virtual private cloud) to launch the cluster into
+aws cloudformation describe-stacks --stack-name $VPC_STACK_NAME
+VPC_EXISTS=$?
+if [ $VPC_EXISTS -ne 0 ]; then
+    aws cloudformation create-stack --stack-name $VPC_STACK_NAME \
+        --template-body https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/amazon-eks-vpc-sample.yaml
+fi
+
+# Wait for stack to create
+aws cloudformation wait stack-create-complete --stack-name $VPC_STACK_NAME
+
+# Get VPC ID
+VPC_ID=$(aws cloudformation describe-stacks --stack-name $VPC_STACK_NAME \
+             | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="VpcId") | .OutputValue')
+
+# Get security group ids
+SECURITY_GROUP_IDS=$(aws cloudformation describe-stacks --stack-name $VPC_STACK_NAME \
+                         | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="SecurityGroups") | .OutputValue')
+
+# Get subnet outputs
+SUBNET_IDS=$(aws cloudformation describe-stacks --stack-name $VPC_STACK_NAME \
+                 | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="SubnetIds") | .OutputValue')
+
+### 2. Create the EKS cluster
+CLUSTER_NAME=$NAME
+ROLE_ARN=arn:aws:iam::${AWSACCT}:role/eksServiceRole
+
+aws eks create-cluster --name $CLUSTER_NAME \
+    --role-arn $ROLE_ARN \
+    --resources-vpc-config subnetIds=$SUBNET_IDS,securityGroupIds=$SECURITY_GROUP_IDS
+
+# Wait for cluster to be created...
+echo "Waiting for EKS cluster to be created... (may take a while)"
+COND=$(aws eks describe-cluster --name $CLUSTER_NAME --query cluster.status)
+while ! [ "$COND" = "\"ACTIVE\"" ]; do
+  sleep 5
+  COND=$(aws eks describe-cluster --name $CLUSTER_NAME --query cluster.status)
+  echo Cluster Status is "$COND"
+done
+echo "EKS cluster created."
+
+# Get cluster endpoint and certificate for configuring kubectl to connect to the
+# cluster
+ENDPOINT=$(aws eks describe-cluster --name $CLUSTER_NAME \
+               --query cluster.endpoint --output text)
+CERTIFICATE_AUTH=$(aws eks describe-cluster --name $CLUSTER_NAME \
+                       --query cluster.certificateAuthority.data --output text)
+
+### 3. Setup kubectl config for connecting to cluster
+mkdir -p ~/.kube
+cp ./kubeconfig.template ~/.kube/config-$CLUSTER_NAME
+sed "s|<endpoint-url>|$ENDPOINT|g" -i ~/.kube/config-$CLUSTER_NAME
+sed "s|<base64-encoded-ca-cert>|$CERTIFICATE_AUTH|g" -i ~/.kube/config-$CLUSTER_NAME
+sed "s|<cluster-name>|$CLUSTER_NAME|g" -i ~/.kube/config-$CLUSTER_NAME
+
+echo "export KUBECONFIG=~/.kube/config-$CLUSTER_NAME:\$KUBECONFIG" >> ~/.bashrc
+export KUBECONFIG=~/.kube/config-$CLUSTER_NAME:$KUBECONFIG
+
+### 4. Create worker nodes
+aws cloudformation create-stack --stack-name $CLUSTER_NAME-workers \
+    --template-body file://scanner-eks-nodegroup.yaml \
+    --capabilities CAPABILITY_IAM \
+    --parameters \
+    ParameterKey=ClusterName,ParameterValue=$CLUSTER_NAME \
+    ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=$SECURITY_GROUP_IDS \
+    ParameterKey=NodeGroupName,ParameterValue=$CLUSTER_NAME-workers-node-group \
+    ParameterKey=NodeAutoScalingGroupMinSize,ParameterValue=1 \
+    ParameterKey=NodeAutoScalingGroupMaxSize,ParameterValue=6 \
+    ParameterKey=NodeInstanceType,ParameterValue=c4.8xlarge \
+    ParameterKey=NodeImageId,ParameterValue=ami-dea4d5a1 \
+    ParameterKey=KeyName,ParameterValue=ISTC-VCS1-JRB \
+    ParameterKey=VpcId,ParameterValue=$VPC_ID \
+    ParameterKey=Subnets,ParameterValue=\"$SUBNET_IDS\"
+
+echo "Waiting for EKS worker node group to be created... (may take a while)"
+aws cloudformation wait stack-create-complete --stack-name $CLUSTER_NAME-workers
+echo "EKS worker node group created."
+
+
+aws cloudformation describe-stacks --stack-name $CLUSTER_NAME-workers
+
+NODE_INSTANCE_ROLE=$(aws cloudformation describe-stacks --stack-name $CLUSTER_NAME-workers \
+             | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="NodeInstanceRole") | .OutputValue')
+
+rm aws-auth-cm.yaml
+curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml
+sed "s*<ARN of instance role (not instance profile)>*$NODE_INSTANCE_ROLE*g" -i aws-auth-cm.yaml
+kubectl apply -f aws-auth-cm.yaml
+
+### 5. Install cloudwatch adapter to support logging to cloud watch
+# HELM is broken glibc versioning problem
+#helm install --name kube2iam stable/kube2iam
+#helm install --name cloudwatch \
+#  --set awsRegion=us-east-1 \
+#  --set awsRole=cloudwatch \
+#    incubator/fluentd-cloudwatch
+
+# Add role binding to allow kube2iam to work correctly
+# See https://github.com/heptio/aws-quickstart/issues/75
+kubectl create clusterrolebinding kube-system-default-admin \
+  --clusterrole=cluster-admin \
+  --serviceaccount=default:default
+
+### 6. Tell master and worker pods about db path
+# Pablo's bucket jhubs3
+sed "s|<CLUSTER_NAME>|$CLUSTER_NAME|g" scanner-config.yaml.template > scanner-config.yml
+kubectl apply -f scanner-config.yml
+
+### 7. Add dashboard (By Blakley)
+#kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+#nohup kubectl proxy&
+#	http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
+## OR: export DASHURL=`kubectl cluster-info|grep "Kubernetes master"|sed '/^.*at /s///;s/$/\/ui/'`
+##TOKEN=$(kubectl -n kube-system get secret kubernetes-dashboard-token-45nhk -o json|jq -r '.data.token')
+#kubectl apply -f dashboard-adminuser.yaml
+
