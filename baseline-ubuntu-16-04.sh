3c8c7195887687354b3f2fc80fb7b4c73478d340
Jim Blakley <jimbla9@gmail.com>
2019-03-17 05:57:19 -0400

    adjust baseline script

--- baseline-ubuntu-16-04.sh
+++ baseline-ubuntu-16-04.sh
@@ -1,6 +1,13 @@
 ## Configure Ubuntu 16.04 Instance AWS for EKS and Scanner
 ## Must be root
 
+# AWS Tools
+apt update &&
+apt-get -y install --upgrade python3-botocore python3-dateutil python3-docutils python3-jmespath python3-roman docutils-common &&
+apt -y install awscli &&
+aws configure &&
+pip3 install awscli --upgrade
+
 # GIT
 
 test -d ~/git || mkdir ~/git
@@ -9,3 +16,6 @@ git clone https://github.com/jblakley/HermesPeak &&
 cd ~/git/HermesPeak/ScannerPG/EKSScannerQS &&
 echo "Now run build_staging_machine.sh" &&
 . ./build_staging_machine.sh
+
+cd ~/git/HermesPeak/ScannerPG/EKSScannerQS
+python3 scanner_EKS_builder.py -c jrbk8sQScluster -n 3 -m 5 --create -d 2>&1 |tee create.log
