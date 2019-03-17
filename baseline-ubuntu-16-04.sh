7bff3c73ec68e20ab4237654b98475190fd3d14a
Jim Blakley <jimbla9@gmail.com>
2019-03-17 09:57:11 -0400

    tweak

--- baseline-ubuntu-16-04.sh
+++ baseline-ubuntu-16-04.sh
@@ -6,18 +6,20 @@ CLUSTERNAME=jrbk8sQScluster
 apt update &&
 apt-get -y install --upgrade python3-botocore python3-dateutil python3-docutils python3-jmespath python3-roman docutils-common &&
 apt -y install awscli &&
+# your credentials, region and json
 aws configure &&
+apt install python3-pip -y &&
 pip3 install awscli --upgrade
 
 # GIT
 
 test -d ~/git || mkdir ~/git
 cd ~/git
-git clone https://github.com/jblakley/HermesPeak &&
+git clone https://github.com/jblakley/HermesPeak
 cd ~/git/HermesPeak/ScannerPG/EKSScannerQS &&
 echo "Now run build_staging_machine.sh" &&
 . ./build_staging_machine.sh
 
 cd ~/git/HermesPeak/ScannerPG/EKSScannerQS
-python3 scanner_EKS_builder.py -c jrbk8sQScluster -n 3 -m 5 --create -d 2>&1 |tee create.log
+python3 scanner_EKS_builder.py -c jrbk8sQScluster2 -n 3 -m 5 --create --deploy -d 2>&1 |tee create.log
 . ./setkubectl.sh
