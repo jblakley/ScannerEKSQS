682fb6f6d7659ae3f745eb51e6d3c6ffb4a16db5
root <root@ip-172-31-22-16.ec2.internal>
2019-03-29 11:16:40 +0000

    Continue ARN implementation

--- scanner_EKS_builder.py
+++ scanner_EKS_builder.py
@@ -143,7 +143,7 @@ def main():
         ''' End Setup '''
 
         ''' App Run '''
-        
+        create_arn(kwargs)
         if deleteCluster is True:
             delete_cluster(kwargs)
             sys.exit(0)
@@ -340,12 +340,14 @@ def create_setK8SSenv(kwargs):
 
 def create_arn(kwargs):
     ''' See if arn exists. If not create '''
-    ARN = "arn:aws:iam::%s:role/eksServiceRole" % kwargs[AWSACCT]
-    arns = sp.check_output(
-        '''
-        aws iam list-roles|jq -q '.Roles[].Arn'
-        ''',
-        shell=True).strip().decode('utf-8')
+    ARN = "arn:aws:iam::%s:role/eksServiceRole" % kwargs['AWSACCT']
+#    ARN = "arn:aws:iam::539776273521:role/aws-service-role/support.amazonaws.com/AWSServiceRoleForSupport"
+    arns = cmd("aws iam list-roles|jq -r '.Roles[].Arn'")
+    for TMPARN in arns: # for some reason, normal "if ARN in" is not working
+        if TMPARN == ARN:
+            print("ARN %s exists" % ARN)
+            return True
+    ''' arn doesn't exist -- create it '''
     pass
 
 def getDBGSTR():
