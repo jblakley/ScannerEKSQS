38e706f8911fe17e39a34de46cc7b2e19bc61880
Jim Blakley <jimbla9@gmail.com>
2019-03-31 17:23:47 -0400

    start work on container based staging machine

---
+++ baseline-ubuntu-out-container.sh
@@ -0,0 +1,38 @@
+## Configure Ubuntu 16.04 Instance AWS for EKS and Scanner
+## Must be root
+apt update
+apt install python3-pip jq -y
+pip3 install tqdm
+
+# Install kubectl
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+
+QSHOME=~/git/HermesPeak/ScannerPG/EKSScannerQS
+
+# AWS Tools
+apt update &&
+apt-get -y install --upgrade python3-botocore python3-dateutil python3-docutils python3-jmespath python3-roman docutils-common &&
+apt -y install awscli &&
+# your credentials, region and json
+
+apt install python3-pip -y &&
+pip3 install awscli --upgrade &&
+aws configure &&
+aws --version
+
+export AWS_ACCESS_KEY_ID=$(grep aws_access_key_id ~/.aws/credentials|awk '{print $3}')
+export AWS_SECRET_ACCESS_KEY=$(grep aws_secret_access_key ~/.aws/credentials|awk '{print $3}')
+
+# GIT
+
+test -d ~/git || mkdir ~/git
+cd ~/git
+test -d HermesPeak || git clone https://github.com/jblakley/HermesPeak
+cd $QSHOME
+
+# Start Container ... TODO
