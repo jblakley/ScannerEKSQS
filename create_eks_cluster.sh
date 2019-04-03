8aa87df6f40fa11c012ecddc3c00d57ebf004132
Pablo Munoz <jpablomch@gmail.com>
2019-04-03 15:45:35 -0700

    Added -f to avoid error in rm aws-auth-cm.yaml if file doesn't exist

--- create_eks_cluster.sh
+++ create_eks_cluster.sh
@@ -110,7 +110,7 @@ aws cloudformation describe-stacks --stack-name $CLUSTER_NAME-workers
 NODE_INSTANCE_ROLE=$(aws cloudformation describe-stacks --stack-name $CLUSTER_NAME-workers \
              | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="NodeInstanceRole") | .OutputValue')
 
-rm aws-auth-cm.yaml
+rm -f aws-auth-cm.yaml
 curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml
 sed "s*<ARN of instance role (not instance profile)>*$NODE_INSTANCE_ROLE*g" -i aws-auth-cm.yaml
 kubectl apply -f aws-auth-cm.yaml
