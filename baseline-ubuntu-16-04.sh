868ce2e77817cb672548d6f11130d5f0c77c93b2
jblakley <jimbla9@gmail.com>
2019-04-06 16:07:23 -0400

    remove creation of  seb_config.json
    Its now in scanner_EKS_builder

--- baseline-ubuntu-16-04.sh
+++ baseline-ubuntu-16-04.sh
@@ -29,17 +29,4 @@ test -d ScannerEKSQS || git clone https://github.com/jblakley/ScannerEKSQS
 
 cd $QSHOME
 
-# AWS Account to create seb configuration
-read -p "Enter your AWS account number: " AWS_ACC
-# For now, we are using this for max and desired nodes.
-read -p "Enter the number of nodes [2]: " NODE_NUM
-NODE_NUM=${NODE_NUM:-2}
-read -p "Enter your region [us-east-1]: " AWS_REGION
-AWS_REGION=${AWS_REGION:-"us-east-1"}
-read -p "Enter the name of the cluster : " CLUSTER_NAME
-read -p "Enter your S3 bucket: " AWS_BUCKET
-read -p "Enter keyname: " KEYNAME
-
-printf "{\n\t\"maxNodes\": ${NODE_NUM},\n\t\"nodesDesired\": ${NODE_NUM},\n\t\"region\":\"${AWS_REGION}\",\n\t\"account\":\"${AWS_ACC}\",\n\t\"clusterName\":\"${CLUSTER_NAME}\",\n\t\"VPC_STACK_NAME\":\"eks-vpc\",\n\t\"CONTAINER_TAG\":\"jpablomch/scanner-aws:latest\",\n\t\"BUCKET\":\"${AWS_BUCKET}\",\n\t\"KEYNAME\":\"${KEYNAME}\"\n}" > seb_config.json
-
 python3 ./scanner_EKS_builder.py --staging
