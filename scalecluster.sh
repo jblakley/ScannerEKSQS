2980524aa1cd218726595874fca34f1f25c901f4
root <root@ip-192-168-86-220.ec2.internal>
2019-04-07 20:51:22 +0000

    fix cluster scaling -- pods up and down

--- scalecluster.sh
+++ scalecluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script will change the number of nodes in the cluster and then adjust the number of worker pods
+# The deployment must be running for this to work
 NEWMAX=$1
+PODDESIRED=$(expr $NEWMAX - 1)
 if [ $NEWMAX -lt 2 ]
 then
 	echo "Number of nodes must be at least 2 -- one for master and one for worker: $NEWMAX"
@@ -12,23 +14,42 @@ echo Changing auto scaling group $ASGNAME to desired capacity $NEWMAX
 aws autoscaling set-desired-capacity --auto-scaling-group-name $ASGNAME --desired-capacity $NEWMAX
 
 NUMINSVC=$(aws autoscaling describe-auto-scaling-groups |jq -r '.AutoScalingGroups[].Instances[].LifecycleState'|wc|awk '{print $1}')
+# Scaling the cluster down
 if [ $NEWMAX -lt $NUMINSVC ]
 then
-	echo "Cluster is smaller than current -- deleting deployment. You will have to redeploy manually"
+	# Scale the pods down first
 	PODINSVC=$(kubectl get pods|egrep -e "worker.*Running"|wc|awk '{print $1}')
-	PODDESIRED = $(expr $NEWMAX - 1)
 	if [ $PODINSVC -gt $PODDESIRED ]
 	then
-		echo "Print reducing the number of worker pods for $PODINSVC to $PODDESIERED"
+		echo "Reducing the number of worker pods from $PODINSVC to $PODDESIRED"
 		kubectl scale deployment/scanner-worker --replicas=$PODDESIRED
+		while [ $PODINSVC -ne $PODDESIRED ]
+		do
+			PODINSVC=$(kubectl get pods|egrep -e "worker.*Running"|wc|awk '{print $1}')
+			kubectl get pods
+			sleep $SLEEP
+		done
 	fi
-	# bash ./delete_deployment.sh # Perhaps overkill TODO
 fi
+# Now scale the nodes
 while [ $NEWMAX -ne $NUMINSVC ]
 do
 	echo "Waiting for number of nodes to stabilize: $NUMINSVC going to $NEWMAX"
 	sleep $SLEEP
 	kubectl get nodes
-	NUMINSVC=$(aws autoscaling describe-auto-scaling-groups |jq -r '.AutoScalingGroups[].Instances[].LifecycleState'|wc|awk '{print $1}')
+	# NUMINSVC=$(aws autoscaling describe-auto-scaling-groups |jq -r '.AutoScalingGroups[].Instances[].LifecycleState'|wc|awk '{print $1}')
+	NUMINSVC=$(kubectl get nodes|grep Ready|grep -v NotRead|wc|awk '{print $1}')
 	# TODO check to make sure that they're actually ready
 done
+
+# Now scale pods -- redundant if scaling down. Already set above
+kubectl scale deployment/scanner-worker --replicas=$PODDESIRED
+echo "Setting the number of worker pods to $PODDESIRED"
+PODINSVC=$(kubectl get pods|egrep -e "worker.*Running"|wc|awk '{print $1}')
+while [ $PODINSVC -ne $PODDESIRED ]
+do
+        sleep $SLEEP
+	PODINSVC=$(kubectl get pods|egrep -e "worker.*Running"|wc|awk '{print $1}')
+        kubectl get pods
+done
+
