17ccb4792d1c25380b6d8fb3ad271326630bdcf5
Pablo Munoz <jpablomch@gmail.com>
2019-04-24 16:06:44 -0700

    Deleted old smoketest.py Set smoketest-v3.py in run_smoke() removed unused role arn in delete script

--- delete_eks_cluster.sh
+++ delete_eks_cluster.sh
@@ -13,7 +13,6 @@ fi
 NAME=$1
 
 CLUSTER_NAME=$NAME
-ROLE_ARN=arn:aws:iam::459065735846:role/eksServiceRole
 
 ### 1. Delete worker nodes
 aws cloudformation delete-stack --stack-name $CLUSTER_NAME-workers
