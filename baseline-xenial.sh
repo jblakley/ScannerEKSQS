111b55ad83d213c1dff00b78d37eb7f84b52902a
jblakley <jblakley@jblakley-MOBL.amr.corp.intel.com>
2019-10-28 14:48:34 -0400

    back to python3.5

--- baseline-xenial.sh
+++ baseline-xenial.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 ## Configure Ubuntu 16.04 Instance AWS for EKS and Scanner
 ## Must be root
-#apt update && apt install software-properties-common -y && add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+
+# Pyhon 3.6, 3.7
+#apt install software-properties-common -y && add-apt-repository ppa:deadsnakes/ppa -y
 #apt update && apt install python3.5 -y && update-alternatives --install /usr/bin/python3 python /usr/bin/python3.6 1
+
+## Python 3.5.8 -- from source
+#apt install -y build-essential checkinstall &&
+#apt install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+#apt remove python3
+#wget https://www.python.org/ftp/python/3.5.8/Python-3.5.8rc1.tgz
+#tar xvzf Python-3.5.8rc1.tgz
+#cd Python-3.5.8rc1
+#./configure && make && make install
+
 apt update
 apt install python3-pip jq git -y
 pip3 install tqdm
